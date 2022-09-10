852f46d032179ac6f48b2b244963f2377c3d467e
Theo Browne <me@t3.gg>
2022-09-10 15:20:56 -0700

    comment out voting to test new fixes

--- src/pages/api/vote.ts
+++ src/pages/api/vote.ts
@@ -1,26 +1,30 @@
-const VOTE_SQL_STRING =
-  "INSERT INTO `roundest-mon`.Vote (id,votedForId,votedAgainstId) VALUES (?, ?, ?)";
+// const VOTE_SQL_STRING =
+//   "INSERT INTO `roundest-mon`.Vote (id,votedForId,votedAgainstId) VALUES (?, ?, ?)";
 
-import { connect } from "@planetscale/database";
-import cuid from "cuid";
+// import { connect } from "@planetscale/database";
+// import cuid from "cuid";
 
-export const makeVote = async (voteFor: number, voteAgainst: number) => {
-  const conn = connect({
-    host: import.meta.env.DATABASE_HOST,
-    username: import.meta.env.DATABASE_USERNAME,
-    password: import.meta.env.DATABASE_PASSWORD,
-  });
-  return await conn.execute(VOTE_SQL_STRING, [cuid(), voteFor, voteAgainst]);
-};
+// export const makeVote = async (voteFor: number, voteAgainst: number) => {
+//   const conn = connect({
+//     host: import.meta.env.DATABASE_HOST,
+//     username: import.meta.env.DATABASE_USERNAME,
+//     password: import.meta.env.DATABASE_PASSWORD,
+//   });
+//   return await conn.execute(VOTE_SQL_STRING, [cuid(), voteFor, voteAgainst]);
+// };
 
-import { z } from "zod";
+// import { z } from "zod";
+// export async function post({ request }) {
+//   const data = await request.json();
+
+//   const validated = z
+//     .object({ voteFor: z.number(), voteAgainst: z.number() })
+//     .parse(data);
+
+//   const res = await makeVote(validated.voteFor, validated.voteAgainst);
+//   return new Response(JSON.stringify({ done: res }));
+// }
+
 export async function post({ request }) {
-  const data = await request.json();
-
-  const validated = z
-    .object({ voteFor: z.number(), voteAgainst: z.number() })
-    .parse(data);
-
-  const res = await makeVote(validated.voteFor, validated.voteAgainst);
-  return new Response(JSON.stringify({ done: res }));
+  return new Response(JSON.stringify({ done: true }));
 }
