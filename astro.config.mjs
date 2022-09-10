fdbdfd24541bc3deeb906a2dfa64cdd98ec8c531
Theo Browne <me@t3.gg>
2022-09-09 22:15:47 -0700

    edge mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind()]
+  integrations: [preact(), tailwind()],
+  output: "server",
+  adapter: vercel(),
 });
