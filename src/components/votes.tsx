7cf63bfc7479939e9a2f97d450d324cdb0e6bf16
Theo Browne <me@t3.gg>
2022-09-09 22:14:08 -0700

    Results page and voting fully mocked

--- src/components/votes.tsx
+++ src/components/votes.tsx
@@ -1,5 +1,43 @@
 import { h, Fragment, FunctionComponent } from "preact";
-import { useState } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
+import { getOptionsForVote, getRandomPokemon } from "../data/getRandom";
+import { ALL_MONS } from "../data/mons";
+
+const btn =
+  "inline-flex items-center px-3 py-1.5 border border-gray-300 shadow-sm font-medium rounded-full text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
+
+const PokemonListing: FunctionComponent<{
+  pokemon: number;
+  vote: () => void;
+  disabled?: boolean;
+}> = (props) => {
+  return (
+    <div
+      className={`flex flex-col items-center transition-opacity ${
+        props.disabled && "opacity-0"
+      }`}
+      key={props.pokemon}
+    >
+      <div className="text-xl text-center capitalize mt-[-0.5rem]">
+        {ALL_MONS[props.pokemon]}
+      </div>
+      <img
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.pokemon}.png`}
+        width={256}
+        height={256}
+        layout="fixed"
+        className="animate-fade-in"
+      />
+      <button
+        className={btn}
+        onClick={() => props.vote()}
+        disabled={props.disabled}
+      >
+        Rounder
+      </button>
+    </div>
+  );
+};
 
 const Voter: FunctionComponent<{ a: number; b: number }> = ({
   children,
@@ -9,11 +47,32 @@ const Voter: FunctionComponent<{ a: number; b: number }> = ({
   const [left, setLeft] = useState(a);
   const [right, setRight] = useState(b);
 
+  // Preload all images
+  useEffect(() => {
+    ALL_MONS.forEach((_, i) => {
+      new Image().src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
+    });
+  }, []);
+
+  // Update vote state
+  const voteForRoundest = (vote: number) => {
+    const antiVote = vote === left ? right : left;
+    console.log("voted", vote, antiVote);
+
+    const newOptions = getOptionsForVote();
+    setLeft(newOptions[0]);
+    setRight(newOptions[1]);
+  };
+
   return (
     <div class="flex flex-col">
       <div class="flex gap-2 text-xl">
-        <div>{left}</div>
-        <div>{right}</div>
+        <div className="p-8 flex justify-between items-center max-w-2xl flex-col md:flex-row animate-fade-in">
+          <PokemonListing pokemon={left} vote={() => voteForRoundest(left)} />
+          <div className="p-8 italic text-xl">{"or"}</div>
+          <PokemonListing pokemon={right} vote={() => voteForRoundest(right)} />
+          <div className="p-2" />
+        </div>
       </div>
       <div class="counter-message">{children}</div>
     </div>
