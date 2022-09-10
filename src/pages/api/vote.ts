91bf96428170da5b4fa2f47c9f0532107b135e8d
Theo Browne <me@t3.gg>
2022-09-10 03:44:24 -0700

    Switch to nanoid because crypto

--- src/pages/api/vote.ts
+++ src/pages/api/vote.ts
@@ -1,21 +1,19 @@
 const VOTE_SQL_STRING =
   "INSERT INTO `roundest-mon`.Vote (id,votedForId,votedAgainstId) VALUES (?, ?, ?)";
 
-// 'INSERT INTO `roundest-mon`.Vote (id,createdAt,votedForId,votedAgainstId) VALUES ("cl7vrh5rq0000glq74e31nguf", "2022-09-10 10:25:24.086 UTC", 22, 447)';
-
 import { connect } from "@planetscale/database";
-import { v4 as uuidv4 } from "uuid";
+import { nanoid } from "nanoid";
 
-import { z } from "zod";
 export const makeVote = async (voteFor: number, voteAgainst: number) => {
   const conn = connect({
     host: import.meta.env.DATABASE_HOST,
     username: import.meta.env.DATABASE_USERNAME,
     password: import.meta.env.DATABASE_PASSWORD,
   });
-  return await conn.execute(VOTE_SQL_STRING, [uuidv4(), voteFor, voteAgainst]);
+  return await conn.execute(VOTE_SQL_STRING, [nanoid(), voteFor, voteAgainst]);
 };
 
+import { z } from "zod";
 export async function post({ request }) {
   const data = await request.json();
 
