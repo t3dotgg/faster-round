cd67ddc408d40082e75e1f0c32280d3a82494775
Theo Browne <me@t3.gg>
2022-09-09 21:39:06 -0700

    add preact and tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [preact(), tailwind()]
+});
